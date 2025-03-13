86bd3b1ed532c6e19a26e8b398f32e9e0cbb8ceb
leogalencar-fatec <leonardo.alencar@fatec.sp.gov.br>
2025-03-13 14:30:38 -0300

    missing csv_table_mapping from previous commit 88a32ab

---
+++ src/constants/csv_table_mapping.py
@@ -0,0 +1,12 @@
+CSV_TABLE_MAPPING = {
+    "cnae": "cnae",
+    "motivo": "motivo",
+    "municipio": "municipio",
+    "natureza": "natureza_juridica",
+    "pais": "pais",
+    "qualifica": "qualificacao_socio",
+    "empresa": "empresa",
+    "estabelecimento": "estabelecimento",
+    "socio": "socio",
+    "simples": "simples",
+}
