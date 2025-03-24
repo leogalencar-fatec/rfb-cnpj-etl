00a80fe1561519699c05c73ce4ce14864e436afe
leogalencar-fatec <leonardo.alencar@fatec.sp.gov.br>
2025-03-24 10:17:51 -0300

    fix: set sql_model on session

--- src/sql/setup_mysql.sql
+++ src/sql/setup_mysql.sql
@@ -8,8 +8,9 @@ SET CHARACTER SET utf8mb4;
 -- Check and set secure_file_priv (optional)
 SHOW VARIABLES LIKE 'secure_file_priv';
 
--- Disable SQL strict mode (mandatory)
+-- Disable SQL strict mode temporary (mandatory)
 SET GLOBAL sql_mode = '';
+SET SESSION sql_mode = '';
 
 -- Create database if not exists (change 'my_database' to correspond src/config/config.yaml)
 CREATE DATABASE IF NOT EXISTS my_database CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
